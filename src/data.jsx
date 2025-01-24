dd5dfd9bdd4875b71e9bfdf1f0a5c0fde13ec5ea
Aayush Thakur <ashcold21@gmail.com>
2025-01-24 15:42:15 +0530

    Update data.jsx to enhance skills section and add new technologies

--- src/data.jsx
+++ src/data.jsx
@@ -1,43 +1,61 @@
-import { nanoid } from 'nanoid';
-import { FaHtml5, FaJs, FaReact, FaJava } from 'react-icons/fa';
-import { SiTypescript } from "react-icons/si";
+import { nanoid } from "nanoid";
+import { FaHtml5, FaJs, FaReact, FaJava, FaNode } from "react-icons/fa";
+import { SiTypescript, SiMongodb, SiMysql } from "react-icons/si";
 
 export const links = [
-  { id: nanoid(), href: '#home', text: 'home' },
-  { id: nanoid(), href: '#skills', text: 'skills' },
-  { id: nanoid(), href: '#about', text: 'about' },
-  { id: nanoid(), href: '#projects', text: 'projects' },
+  { id: nanoid(), href: "#home", text: "home" },
+  { id: nanoid(), href: "#skills", text: "skills" },
+  { id: nanoid(), href: "#about", text: "about" },
+  { id: nanoid(), href: "#projects", text: "projects" },
 ];
 
 export const skills = [
   {
     id: nanoid(),
-    title: 'HTML&CSS',
-    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
-    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+    title: "HTML&CSS",
+    icon: <FaHtml5 className="h-16 w-16 text-emerald-500" />,
+    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
   },
   {
     id: nanoid(),
-    title: 'Javascript',
-    icon: <FaJs className='h-16 w-16 text-emerald-500' />,
-    text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
+    title: "Javascript",
+    icon: <FaJs className="h-16 w-16 text-emerald-500" />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
   },
   {
     id: nanoid(),
-    title: 'React',
-    icon: <FaReact className='h-16 w-16 text-emerald-500' />,
-    text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
+    title: "Typescript",
+    icon: <SiTypescript className="h-16 w-16 text-emerald-500" />,
+    text: "Knowledge of the fundamental concepts of Typescript which would help to create type-safe and more bug resilient codebase.",
   },
   {
     id: nanoid(),
-    title: 'Typescript',
-    icon: <SiTypescript className='h-16 w-16 text-emerald-500' />,
-    text: 'Knowledge of the fundamental concepts of Typescript which would help to create type-safe and more bug resilient codebase.',
+    title: "React",
+    icon: <FaReact className="h-16 w-16 text-emerald-500" />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
   },
   {
     id: nanoid(),
-    title: 'Java',
-    icon: <FaJava className='h-16 w-16 text-emerald-500' />,
-    text: 'Good grasp of Java including basics and core concepts of Java in order to write fast and maintainable code.',
+    title: "Java",
+    icon: <FaJava className="h-16 w-16 text-emerald-500" />,
+    text: "Good grasp of Java including basics and core concepts of Java in order to write fast and maintainable code.",
+  },
+  {
+    id: nanoid(),
+    title: "Node.js",
+    icon: <FaNode className="h-16 w-16 text-emerald-500" />,
+    text: "Proficient in Node.js, building scalable and efficient back-end applications with a focus on asynchronous programming and event-driven architecture.",
+  },
+  {
+    id: nanoid(),
+    title: "MongoDB",
+    icon: <SiMongodb className="h-16 w-16 text-emerald-500" />,
+    text: "Experience in MongoDB, creating and managing databases to store and retrieve data for web applications.",
+  },
+  {
+    id: nanoid(),
+    title: "MySQL",
+    icon: <SiMysql className="h-16 w-16 text-emerald-500" />,
+    text: "Knowledge of MySQL, designing and managing relational databases to store and retrieve data for web applications.",
   },
 ];
